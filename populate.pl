1b8b401e3ae199d0b821cc4956b99ba601002636
Tony Edwardson <tony@edwardson.co.uk>
2024-03-17 10:59:41 +0000

    WIP

--- populate.pl
+++ populate.pl
@@ -3,50 +3,75 @@
 # a new directory and uploads all html files in 
 # the /tmp directory.
  
+use feature 'say';
+use strict;
+use warnings;
+use HTTP::Tiny;
+use HTML::TreeBuilder;
+use Data::Dumper;
 use HTTP::DAV;
+use FileHandle;
  
-$d = HTTP::DAV->new();
-$url = "http://TonyEdwdsonsPro:1980/";
+my $d = HTTP::DAV->new();
+HTTP::DAV::DebugLevel(3);
+
+my $url = "http://192.168.178.56:1980/greenleysttc";
  
 $d->credentials(
    -user  => "admin",
    -pass  => "admin", 
-   -url   => $url,
-   -realm => "DAV Realm"
-);
+   -realm => 'Zope',
+   -url   => $url)
+   or die("Couldn't set cred : " .$d->message . "\n");
+
  
-my $teams = {
-    
-Name	Captain	Division	Home night	Fixtures
-Crusaders	Ian Grainger	Division Four	Tuesday	2023/24
-Dukes	David Fletcher	Division Four	Tuesday	2023/24
-Glory	Jacob Midson	Premier Division	Tuesday	2023/24
-Knights	Thomas Reddall	Division Five	Wednesday	2023/24
-Moghuls	Orrin Edwards	Division Three	Tuesday	2023/24
-Monarchs	Martin Hall	Premier Division	Wednesday	2023/24
-Princes	Saravana Kumar	Division One	Wednesday	2023/24
-Sovereigns	Stefan Swift	Division Six	Tuesday	2023/24
-Sultans of Spin	Tony Edwardson	Division Two	Wednesday	2023/24
-Valiants	Esther Carter	Division Two	Tuesday	2023/24
-Warriors	Ricky Taiwo	Premier Division	Wednesday	2023/24
-Zainabiya	Faraz Mohsin	Division Four	Wednesday	2023/24
-};
 $d->open( -url => $url )
    or die("Couldn't open $url: " .$d->message . "\n");
  
+my $front_page = "$url/front-page";
+#$d->unlock( -url => $front_page );
 # Make a null lock on newdir
-$d->lock( -url => "$url/newdir", -timeout => "10m" ) 
-   or die "Won't put unless I can lock for 10 minutes\n";
- 
+#$d->lock( -url => $front_page, -timeout => "10m" ) 
+#   or die "Won't put unless I can lock for 10 minutes\n";
+
+my $html;
+$d->get($front_page, \$html);
+save_data("newsite.html", $html);
+my $table = scrape('http://www.mkttl.co.uk/clubs/greenleys'); 
+$html =~ s/^<div.*$/$table/;
+save_data("newpage.html", $html);
 # Make a new directory
-$d->mkcol( -url => "$url/newdir" )
-   or die "Couldn't make newdir at $url\n";
  
 # Upload multiple files to newdir.
-if ( $d->put( -local => "/tmp/*.html", -url => $url ) ) {
+if ( $d->put( -local => "newpage.html", -url => $url ) ) {
    print "successfully uploaded multiple files to $url\n";
 } else {
    print "put failed: " . $d->message . "\n";
 }
  
-$d->unlock( -url => $url );
+$d->unlock( -url => $front_page );
+
+sub scrape {
+	my ($url) = @_;
+
+	my $http = HTTP::Tiny->new();
+	my $response = $http->get($url);
+	my $tree = HTML::TreeBuilder->new_from_content( $response->{content} );
+	#say $response->{content};
+	#my $e = $tree->look_down('table', 'datatable');
+	my $table = $tree->look_down(
+		sub {
+	    # the lcs are to fold case
+	    lc($_[0]->tag()) eq 'table'
+		    and lc($_[0]->attr('id')) eq 'datatable'
+	  }
+	);
+	save_data("mainsite.html", $table->as_HTML);
+	return $table->as_HTML;
+}
+sub save_data {
+	my ($filename, $content) = @_;
+	my $fh = FileHandle->new(">$filename") or die "Can't open $filename: $!";
+	$fh->print($content);
+	$fh->close();
+}
--- scraper.pl
+++ scraper.pl
@@ -1,11 +1,21 @@
 #!/usr/bin/env perl
+use feature 'say';
 use strict;
 use warnings;
 use HTTP::Tiny;
 use HTML::TreeBuilder;
+use Data::Dumper;
 
 my $http = HTTP::Tiny->new();
 my $response = $http->get('https://www.mkttl.co.uk/clubs/greenleys');
-
-my $html_content = $response->{content};
-print $html_content;
+my $tree = HTML::TreeBuilder->new_from_content( $response->{content} );
+#say $response->{content};
+#my $e = $tree->look_down('table', 'datatable');
+my $table = $tree->look_down(
+	sub {
+    # the lcs are to fold case
+    lc($_[0]->tag()) eq 'table'
+	    and lc($_[0]->attr('id')) eq 'datatable'
+  }
+);
+say $table->as_HTML;
